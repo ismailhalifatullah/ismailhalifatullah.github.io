3a67a140ac3bc79406efed512d078157cbf2ddfc
ismailhalifatullah <74280556+ismailhalifatullah@users.noreply.github.com>
2020-11-24 09:46:00 +0700

    adif

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuHamberger = document.querySelector('.menu_hamburger input');
-const nav = document.querySelector('nav ul');
-
-menuHamberger.addEventListener('click', function(){
-	nav.classList.toggle('slide');
-});
