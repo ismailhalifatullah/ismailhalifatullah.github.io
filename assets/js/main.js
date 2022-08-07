cd9185b7aefd58f89da0dcd371998d473cf3138a
ismail halifatullah <ismailhalifatullah@gmail.com>
2022-08-07 19:31:56 +0700

    baru gambar

--- assets/js/main.js
+++ assets/js/main.js
@@ -182,7 +182,7 @@
     }
   });
 
-  // Porfolio isotope and filter
+  // Layanan isotope and filter
   $(window).on('load', function () {
     var portfolioIsotope = $('.portfolio-container').isotope({
       itemSelector: '.portfolio-item',
@@ -191,6 +191,29 @@
 
     $('#portfolio-flters li').on('click', function () {
       $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $(document).ready(function () {
+      $('.venobox').venobox();
+    });
+  });
+
+  // jadwal dokter isotope and filter
+  $(window).on('load', function () {
+    var portfolioIsotope = $('.jadwal-container').isotope({
+      itemSelector: '.jadwal-item',
+      layoutMode: 'fitRows'
+    });
+
+    $('#jadwal-filters li').on('click', function () {
+      $("#jadwal-filters li").removeClass('filter-active');
       $(this).addClass('filter-active');
 
       portfolioIsotope.isotope({
